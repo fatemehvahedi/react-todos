32e8f3ea3665346f7b9c09ec983b0dc64ce69132
fatemehvahedi <32917002+fatemehvahedi@users.noreply.github.com>
2018-01-18 14:41:30 +1100

    init git

---
+++ public/src/app.js
@@ -0,0 +1,8 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import 'normalize.css/normalize.css'
+// import './styles/styles.scss';
+
+// ReactDOM.render(<p>this is new App</p>, document.getElementById('app'));
+ 
+console.log('hi');   
